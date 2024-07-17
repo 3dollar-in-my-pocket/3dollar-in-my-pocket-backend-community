1da31bde2b89bbf4c27c5d9e4f6209ef8fda34e2
yerimkoko <gochi97@naver.com>
2024-07-17 23:58:24 +0900

    [TDC-126] 날짜 ISO8601 형식으로 나갈 수 있도록

--- threedollar-application/src/main/java/com/threedollar/ThreedollarServiceApplication.java
+++ threedollar-application/src/main/java/com/threedollar/ThreedollarServiceApplication.java
@@ -3,9 +3,7 @@ package com.threedollar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableWebMvc
 @EnableCaching
 @SpringBootApplication(scanBasePackages = {"com.threedollar"})
 public class ThreedollarServiceApplication {
